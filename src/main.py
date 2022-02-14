a6dd6284a1999db18c5ea4038ac7d62852e9c861
mael.boutonnet <58459551+maelboutonnet@users.noreply.github.com>
2022-02-14 20:26:51 +0100

    Main game loop
    Ajout d'une vraie game loop sur 40 Jours pour trouver la cure au virus zombie

--- src/main.py
+++ src/main.py
@@ -1,29 +1,51 @@
 import random
 
-guessesTaken = 0
+nb_of_citizen = 40
+research_completion_rate = 0
+NB_OF_DAYS_MAX = 40
 
-print('First, your name ?')
-name = input()
+for day in range(NB_OF_DAYS_MAX):
+    print('It\'s day #' + str(day) + ' of the zombie apocalypse')
+    print('There are only ' + str(NB_OF_DAYS_MAX - day) + ' days left to find the cure')
+    print('Cure completion: ' + str(research_completion_rate) + '%')
 
-toGuess = random.randint(1,20)
-print('hi ' + name + ' now its time to guess the number')
+    print('How many citizen will protect the town ?')
+    nb_of_defenders = int(input())
 
+    while(nb_of_defenders > nb_of_citizen):
+        print('You only have ' + str(nb_of_citizen) +' citizens, how many will protect the town ?')
+        nb_of_defenders = int(input())
 
-for guessesTaken in range(6):
-    print('your guess ?')
-    guess = input()
-    guess = int(guess)
+    nb_of_scientists = nb_of_citizen - nb_of_defenders
+    print('Summary :')
+    print(str(nb_of_defenders) + ' citizens will defend the town')
+    print(str(nb_of_scientists) + ' will search a cure')
 
-    if (guess < toGuess):
-        print('you\'re too low')
+    nb_of_zombies = random.randint(1, day+2)
+    print(str(nb_of_zombies) + ' detected nearby')
 
-    if (guess > toGuess):
-        print('you\'re too high')
+    def_score = 0
+    for human_count in range(nb_of_defenders):
+        def_score += random.randint(1,4)
 
-    if (guess == toGuess):
+    dead_people = nb_of_zombies - def_score
+
+    if (dead_people > 0):
+        print('Your defenses were too weak ! Zombies are in the town')
+        print(str(dead_people) + ' were killed during the night')
+    else:
+        print('Your defenses held on during the night !')
+        print('Your citizens are safe for today')
+
+    nb_of_scientists -= dead_people
+
+    if (nb_of_scientists > 0):
+        for human_count in range(nb_of_scientists):
+            research_completion_rate += random.randint(0,2)
+
+    if (research_completion_rate >= 100):
+        print('Congratulation, the cure is complete!')
         break
 
-if (guess == toGuess):
-    print('you won !!')
-else:
-    print('I\m sorry, but you failed. You should have guessed ' + str(toGuess))
+    if (day == NB_OF_DAYS_MAX):
+        print('Sadly, you didn\'t find the cure in time :(')
